a889c4b4e087c6f342ee32100b6f1962acf6c37d
Bhupendra Kshatri <aryan.kshatri@gmail.com>
2026-01-31 14:08:03 +0000

    BugFixes

--- force-app/main/default/lwc/contactListItem/contactListItem.js
+++ force-app/main/default/lwc/contactListItem/contactListItem.js
@@ -9,8 +9,12 @@ export default class ContactListItem extends LightningElement {
 
     handleClick(event) {
         // Read about event best practices at http://developer.salesforce.com/docs/component-library/documentation/lwc/lwc.events_best_practices
+        const contactId = this.contact?.Id;
+        if (!contactId) {
+            return;
+        }
         const selectEvent = new CustomEvent('select', {
-            detail: this.contact.Id
+            detail: contactId
         });
         // Fire the custom event
         this.dispatchEvent(selectEvent);
